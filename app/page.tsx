34f517b065e39e6da3b68f66f5afb2f9a6b3d88b
i8d8e8a8 <i8d8e8a8@gmail.com>
2026-08-03 11:20:56 +0900

    Use dramatic monochrome surgeon portrait

--- app/page.tsx
+++ app/page.tsx
@@ -938,7 +938,7 @@ export default function Home() {
         <div className="shell doctor-grid">
           <div className="doctor-photo">
             <Image
-              src={`${ASSET_BASE}/doctor-philosophy-surgery-v13-bw.png`}
+              src={`${ASSET_BASE}/doctor-philosophy-surgery-v15-karsh.png`}
               alt="수술 확대경을 착용하고 정밀 수술을 진행하는 강태진 대표원장"
               fill
               sizes="(max-width: 760px) 100vw, 44vw"
